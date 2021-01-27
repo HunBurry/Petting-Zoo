30b7f0030dfdb681236b2ce964aecaf6f4faccd5
Hunter Berry <htberry99@gmail.com>
2021-01-27 13:44:05 -0500

    Create a_tiny_watch_dog.ino

---
+++ a_tiny_watch_dog/a_tiny_watch_dog.ino
@@ -0,0 +1,16 @@
+/*
+ * "Watch Dog"
+ * This animal reacts to loud sounds by turning to face them,
+ * acting as a guard dog or watch dog. 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
